f283653b3ccf1493ad85da969221a5e44991f636
AbrahamEmilio <emeilio97@outlook.com>
2024-04-27 17:38:29 -0600

    nuevo

---
+++ main.js
@@ -0,0 +1,68 @@
+const pokemonsContainer = document.querySelector('#pokemons');
+
+function pokemons(i){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then((res) => res.json())
+    .then((e) => {
+
+
+        const card = document.createElement('div');
+        const pokemon = document.createElement('img');
+        const pokemonName = document.createElement('h2');
+        const abilitiesCont = document.createElement('div');
+        const abilityTitle = document.createElement('p');
+        const abilityList = document.createElement('ul');
+        const closed = document.createElement('img');
+
+        card.classList.add('card');
+        pokemon.classList.add('pokemon');
+        pokemonName.classList.add('pokemonName');
+        abilitiesCont.classList.add('abilitiesCont');
+        abilityTitle.classList.add('abilityTitle');
+        abilityList.classList.add('abilityList');
+        closed.classList.add('closed')
+
+
+        pokemon.src = e.sprites.front_default;
+        pokemonName.textContent = e.name;
+
+        const totalAbilities = e.abilities;
+        totalAbilities.forEach((ab) => {
+
+            console.log(ab.ability.name)
+
+            const ability = document.createElement('p');
+            const abilityElement = document.createElement('li')
+
+            ability.classList.add('ability');
+            abilityElement.classList.add('abilityElement');
+
+            ability.textContent = ab.ability.name;
+            abilityElement.append(ability)
+            abilityList.append(abilityElement);
+
+        })
+
+        abilityTitle.textContent = 'Habilities';
+
+        abilitiesCont.append(abilityTitle, abilityList)
+        card.append(closed, pokemon, pokemonName, abilitiesCont)
+
+        closed.classList.add('inactive');
+        pokemonName.classList.add('inactive');
+        abilitiesCont.classList.add('inactive');
+
+        pokemon.addEventListener('click', () => {
+            closed.classList.toggle('inactive');
+            pokemonName.classList.toggle('inactive');
+            abilitiesCont.classList.toggle('inactive');
+        })
+
+        pokemonsContainer.append(card);
+
+    })
+}
+
+for(let i = 80; i < 92; i++){
+    pokemons(i);
+}
